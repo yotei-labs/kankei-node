ca6aaccbd02b285380364c70ff8c603b0df5d6b8
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2025-07-11 01:14:28 -0300

    feat: add react-email support

--- src/email/email.ts
+++ src/email/email.ts
@@ -1,11 +1,17 @@
+import { ReactNode } from "react";
 import { ErrorResponse, RequireAtLeastOne } from "../interface";
 import { Kankei } from "../kankei";
 
 interface CreateEmailType {
   /**
-   * The HTML of the email
+   * The HTML for the email
    */
   html: string;
+
+  /**
+   * The React Email component for the email
+   */
+  react: ReactNode;
 }
 
 interface CreateEmailBaseValues {
@@ -43,12 +49,37 @@ export class Email {
   constructor(private readonly kankei: Kankei) {}
 
   /**
+   * Generate HTML from ReactNode component using ReactEmail render
+   *
+   * We need to declare this function because @react-email/render
+   * is only needed for the react field in the create() function, so we import it only if necessary.
+   *
+   * @param node The ReactNode component to render
+   */
+  private render?: (node: ReactNode) => Promise<string>;
+
+  /**
    * Create and send email
    *
    * @param values The email data
    * @returns The created email
    */
   public async create(values: CreateEmailValues): Promise<CreateEmailResponse> {
+    if (values.react) {
+      if (!this.render) {
+        try {
+          const { render } = await import("@react-email/render");
+          this.render = render;
+        } catch (error) {
+          throw new Error(
+            "Failed to render React component. Make sure to install '@react-email/render'"
+          );
+        }
+      }
+
+      values.html = await this.render(values.react);
+    }
+
     const data = await this.kankei.post<CreateEmailResponseSuccess>(
       "/email",
       values
